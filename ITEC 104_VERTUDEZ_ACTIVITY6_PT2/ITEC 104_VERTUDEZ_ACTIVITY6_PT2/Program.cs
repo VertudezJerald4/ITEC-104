4fa7e3e37205673fad2c9bdac3bc59e311706b07
VertudezJerald4 <112453807+VertudezJerald4@users.noreply.github.com>
2022-10-19 00:48:35 -0700

    Add files via upload

---
+++ ITEC 104_VERTUDEZ_ACTIVITY6_PT2/ITEC 104_VERTUDEZ_ACTIVITY6_PT2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+  
+class GFG {
+  
+    
+    public static void Main()
+    {
+  
+        
+        HashSet<string> mySet1 = new HashSet<string>();
+  
+        
+        HashSet<string> mySet2 = new HashSet<string>();
+  
+        
+        mySet1.Add("Hello");
+        mySet1.Add("Geeks");
+        mySet1.Add("GeeksforGeeks");
+  
+        
+        mySet2.Add("Geeks");
+        mySet2.Add("and");
+        mySet2.Add("GeeksforGeeks");
+        mySet2.Add("are");
+        mySet2.Add("the");
+        mySet2.Add("best");
+  
+        
+        HashSet<string> ans = new HashSet<string>(mySet1);
+  
+        ans.IntersectWith(mySet2);
+  
+        
+        foreach(string i in ans)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
