e10a3c5eca5856eb4e1985cfebb319ac7fd7a16c
VertudezJerald4 <112453807+VertudezJerald4@users.noreply.github.com>
2022-10-18 21:17:35 -0700

    Add files via upload

---
+++ ITEC 104_VERTUDEZ_ACTIVITY5/ITEC 104_VERTUDEZ_ACTIVITY5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ITEC_104_VERTUDEZ_ACTIVITY5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<int> mylist = new List<int> {10, 20, 35 , 65, 53, 48 , 5, 1};
+			 
+			List<int> Result = mylist.Where(x => x % 10 != 5).ToList();
+			foreach(var element in Result)
+			{
+				Console.WriteLine(element + "");
+			}
+			Console.ReadLine();
+		}
+	}
+}
